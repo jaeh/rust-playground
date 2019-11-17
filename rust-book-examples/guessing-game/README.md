596a4ab4455a1f8b42e96239b33d2ca0f883e619
Jascha <jascha@jaeh.at>
2019-11-17 21:10:05 +0100

    add guessing game rust book example without random

---
+++ rust-book-examples/guessing-game/README.md
@@ -0,0 +1,5 @@
+#### Guessing game
+
+part of the rust book examples.
+
+extended by checking random number for 0-100 bounds.
---
+++ rust-book-examples/guessing-game/src/main.rs
@@ -0,0 +1,39 @@
+use std::io;
+use std::cmp::Ordering;
+// use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    // let secret_number : u32 = rand::thread_rng().gen_range(0, 100);;
+    let secret_number : u32 = 32;
+
+    loop {
+        println!("Please input a number between 0 and 100.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess > 100 {
+            panic!("guess is too big, maximum is 100.");
+        }
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
