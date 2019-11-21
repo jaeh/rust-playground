15a39fd9b4f00ea14ef0bca5e1a9625d31adae4d
Jascha <jascha@jaeh.at>
2019-11-21 21:01:00 +0100

    move index.js to src

--- wasm/hello-world/src/index.js
+++ wasm/hello-world/src/index.js
@@ -1,7 +1,7 @@
 // Note that a dynamic `import` statement here is required due to
 // webpack/webpack#6615, but in theory `import { greet } from './pkg/hello_world';`
 // will work here one day as well!
-const rust = import('./pkg/hello_world');
+const rust = import('../pkg/hello_world');
 
 // es5 promises
 // rust
