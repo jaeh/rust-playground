7eee31497cddbc930ff402b5aecbf579bf0090ac
Jascha <jascha@jaeh.at>
2019-11-27 17:59:28 +0100

    update for usage without webpack

--- wasm/sha3-512-wasm/src/index.js
+++ wasm/sha3-512-wasm/src/index.js
@@ -1,16 +1,28 @@
-const rust = import('../pkg/sha3_512_wasm')
+import init, { hash } from '/pkg/sha3_512_wasm.js';
 
 const run = async () => {
-  try {
-    const m = await rust
-    const world = await m.hash('world')
-    console.log('m.hash("world") returned: ', world);
-    const world2 = await m.hash('world')
-    console.log('m.hash("world") returned: ', world2);
+  await init("/pkg/sha3_512_wasm_bg.wasm");
+  
+  const testingString = "Hail Eris!"
+  const testingString2 = "All hail discordia!"
 
-    console.log('equal hashes:', world === world2)
+  const hashedTesting = hash(testingString)
+  console.log(`"${testingString}", hashed for the first time: "${hashedTesting}"`)
 
-    const str = `
+  const hashedTesting2 = hash(testingString)
+  console.log(`${testingString}, hashed again, ${hashedTesting2}`)
+
+  console.log(hashedTesting === hashedTesting2, `hashes for "${testingString}" are equal`)
+
+  const testingString2Hash = hash(testingString2)
+  console.log(`hashed "${testingString2}", ${testingString2Hash}`)
+
+  const testingString2Hash2 = hash(testingString2)
+  console.log(`hashed "${testingString2}" again, ${testingString2Hash2}`)
+
+  console.log(testingString2Hash === testingString2Hash2, `hashes for ${testingString2} are equal`)
+
+  const loremIpsum = `
     Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
     when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
@@ -19,20 +31,8 @@ const run = async () => {
     It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
     and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
 
-    const str_hash = await m.hash(str)
-    console.log('m.hash("str") returned: ', str_hash)
-
-  //   const resultDiv = document.getElementById('resultworld')
-  //   resultDiv.innerHTML = `
-  //   1. m.hash("world") returned: ${world}
-  //   <br /> 
-  //   2. m.hash("world") returned: ${world2}
-  //   <br />
-  //   3. m.hash(${str}) returned: ${str_hash}  
-  // ` 
-  } catch (e) {
-    console.error(e)
-  }
+  const loremIpsumHash = await hash(loremIpsum)
+  console.log('hash("lorem ipsum....") returned: ', loremIpsumHash )
 }
 
 run()
--- wasm/sha3-512-wasm/src/lib.rs
+++ wasm/sha3-512-wasm/src/lib.rs
@@ -1,25 +1,16 @@
+use sha3::{Digest, Sha3_512};
 use wasm_bindgen::prelude::*;
 
-use sha3::{Digest, Sha3_512};
-
 #[wasm_bindgen]
-pub fn hash(val: &str) -> String {
-  // create a SHA3-512 object
-  let mut hasher = Sha3_512::new();
+pub fn hash(val: &str) -> JsValue {
+    // create a SHA3-512 object
+    let mut hasher = Sha3_512::new();
 
-  // write input message
-  hasher.input(val);
+    // write input message
+    hasher.input(val);
 
-  // read hash digest
-  let result = hasher.result();
+    // read hash digest
+    let result = hasher.result();
 
-  format!("{:x}", result)
-}
-
-#[test]
-fn test_ohai() {
-    let result = hash("hello world");
-    let expected = hash("hello world");
-
-    assert_eq!(result.len(), expected.len());
+    format!("{:x}", result).into()
 }
