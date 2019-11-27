b82e3030d25a63cd825be2bd6e3ec0abc13671a1
Jascha <jascha@jaeh.at>
2019-11-27 17:59:46 +0100

    update test imports

--- wasm/sha3-512-wasm/tests/app.rs
+++ wasm/sha3-512-wasm/tests/app.rs
@@ -1,4 +1,4 @@
-use wasm_bindgen_test::{wasm_bindgen_test_configure, wasm_bindgen_test};
+use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
 
 wasm_bindgen_test_configure!(run_in_browser);
 
